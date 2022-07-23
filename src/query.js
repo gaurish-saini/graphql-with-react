7ac91207843d10038e33202bc54863e52e374403
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2022-07-23 07:31:33 +0530

    list of repos & search query

---
+++ src/query.js
@@ -0,0 +1,26 @@
+const githubQuery = {
+  query: `
+  {
+    viewer {
+      name
+    }
+    search(
+      query: "user:gaurish-saini sort:updated-desc",
+      type: REPOSITORY,
+      first: 10
+    ) {
+      nodes {
+        ... on Repository {
+          name
+          description
+          id
+          url
+          viewerSubscription
+        }
+      }
+    }
+  }
+ `,
+};
+
+export default githubQuery;
