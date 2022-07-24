8cb266243b975b835c2a57045b376b7ed3a7f5f0
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2022-07-25 01:45:02 +0530

    changes

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+  baseURL: "https://api.github.com/graphql",
+  username: "gaurish-saini",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "bearer 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",
+  },
+};
+
+export default github;
